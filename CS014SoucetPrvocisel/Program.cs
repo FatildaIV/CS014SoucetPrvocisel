0f52a7736f29bafe1143727a2093fc109e6da88f
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:49:29 +0200

    Initial commit

---
+++ CS014SoucetPrvocisel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CS014SoucetPrvocisel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Součet prvních 500 prvočísel:");
+            long soucet = 0;
+            int pocet = 0;
+            int cislo = 2;
+            while (pocet < 500)
+            {
+                if (JePrvocislo(cislo))
+                {
+                    soucet += cislo;
+                    Console.WriteLine("Číslo: {0}\nSoučet: {1}\n", cislo, soucet);
+                    pocet++;
+                }
+                cislo++;
+            }
+            Console.WriteLine("\nVýsledný součet: {0}",soucet);
+            Console.ReadKey();
+        }
+        public static bool JePrvocislo(int cislo)
+        {
+            int x = (int)Math.Floor(Math.Sqrt(cislo));
+            if (cislo == 1) return false;
+            if (cislo == 2) return true;
+            for (int i = 2; i <= x; i++)
+                if (cislo % i == 0) return false;
+            return true;
+        }
+    }
+}
